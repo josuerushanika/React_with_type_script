5bfdc834b6b0117a422c8b6e6a981f3f0661346b
josuerushanika <josuerushanika@gmail.com>
2023-09-07 23:07:06 +0200

    event handleling

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,22 +1,19 @@
 function ListGroup() {
   let items = ["Kinshasa", "Sanfroncisco", "Bakavu", "Kinshasa9"];
 
-  items = [];
-
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1> 
-        <p> No items found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p> No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item,index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
