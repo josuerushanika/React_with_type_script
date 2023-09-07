72f797530630ec3576f1aafa42d8b7689a5792de
josuerushanika <josuerushanika@gmail.com>
2023-09-07 14:51:28 +0200

    Redering function with map

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
+
 function ListGroup() {
+ const items = [
+    'Kinshasa',
+    'Sanfroncisco',
+    'Bakavu',
+    'Kinshasa'  
+ ];
+
+ 
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+        <h1>List</h1>
+        <ul className="list-group">
+        {items.map(item => <li>{item}</li>)}
+        </ul>
+    </>
   );
 }
 
