d70c1781a68e608f635e8c1b1a780371249bd1d2
josuerushanika <josuerushanika@gmail.com>
2023-09-08 18:45:38 +0200

    Last Update

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
+import Like from "./components/Like";
 
 function App() {
-   const [alertVisible, setAlertVisibility] = useState(false);
 
   return <div>
-    { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-    <Button color="primary" onClick={() => setAlertVisibility(true)}>
-       My Button
-    </Button>
+    <Like onClick={() => console.log('clicked')}/>
   </div>;
 }
 
