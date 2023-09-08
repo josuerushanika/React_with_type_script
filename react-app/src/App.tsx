5acf3f94bd07eb1e6f1b884f76f69eb3c9aa6fe7
josuerushanika <josuerushanika@gmail.com>
2023-09-08 10:38:05 +0200

    Add button in app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-
-
   return <div>
-    <Alert>
-      Hello <span> World</span>
-    </Alert>
-  </div>
+    <Button>
+      
+    </Button>
+  </div>;
 }
 
 export default App;
