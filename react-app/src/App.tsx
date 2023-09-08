8a779b67564aacce75758750e7c8085731ed9fea
josuerushanika <josuerushanika@gmail.com>
2023-09-08 13:16:04 +0200

    Update files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+   const [alertVisible, setAlertVisibility] = useState(false);
+
   return <div>
-    <Button color="primary" onClick={() => console.log('Clicked')}>
+    { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+    <Button color="primary" onClick={() => setAlertVisibility(true)}>
        My Button
     </Button>
   </div>;
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,14 +1,26 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
+  onClose: () => void;
 }
 
-
-const Alert = ( {children}: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className='alert alert-primary'>{children}</div>
-  )
-}
+    <>
+      <div className="alert alert-primary alert-dismissible">{children}
+      
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>      
+      </div>
+      
+    </>
+  );
+};
 
-export default Alert
+export default Alert;
