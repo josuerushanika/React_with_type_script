6145e374ac2fd228953ff49e3855072fb7bc01ad
josuerushanika <josuerushanika@gmail.com>
2023-09-10 01:25:24 +0200

    Update files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
-import Cart from "./components/Cart"; 
-import { NavBar } from "./components/NavBar";
-
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-const [cartItems, setCartItems] =  useState(['Product1', 'Product2', 'Maneno']);
-
-  return <div>
-      <NavBar cartItemsCount={cartItems.length}/>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
-  </div>;
+  return (
+    <div>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam labore
+        enim id similique optio fugiat praesentium quidem obcaecati iste unde,
+        odio assumenda tempore quo qui eveniet maxime veniam? Nesciunt, a animi
+        cumque nihil tenetur minus debitis. Illo tempora quia commodi
+        voluptatibus qui doloribus ipsam hic, veritatis adipisci perspiciatis
+        laborum quo laboriosam nemo cum consequuntur nihil ut quaerat odio dolor
+        ratione dicta ad aut fuga? Alias laboriosam odio eius, debitis enim
+        perspiciatis nemo eum reprehenderit. Eos hic cum maiores atque, debitis
+        dolorum a provident nostrum nihil aperiam laboriosam est harum enim
+        repellat fugit at iure officiis quo eius neque ullam ipsa.
+      </ExpandableText>
+    </div>
+  );
 }
 
 export default App;
