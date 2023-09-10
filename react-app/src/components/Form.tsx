5c379d6fcf612f90758724b877c841e02e5cb827
josuerushanika <josuerushanika@gmail.com>
2023-09-11 00:46:11 +0200

    Update files

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -16,7 +16,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -47,7 +47,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button  disabled={!isValid} className="btn btn-primary">Submit</button>
     </form>
   );
 };
