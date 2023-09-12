ed7e706c5b7da9571830680d5968a47e8fb52871
josuerushanika <josuerushanika@gmail.com>
2023-09-12 17:32:48 +0200

    Update files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,15 +7,26 @@ interface User {
 }
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]); 
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-    .then(res => setUsers(res.data));
-  }, [])
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
 
   return (
-   <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
+    <>
+      <p className="text-danger">{error}</p>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
