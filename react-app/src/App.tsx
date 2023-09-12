0e53547494224fe62a0d366da37d3043e52d5fdd
josuerushanika <josuerushanika@gmail.com>
2023-09-12 15:46:41 +0200

    Update files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,21 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-
-  const [category, setCategory] = useState('');
-  const ref = useRef<HTMLInputElement>(null);
+  const [users, setUsers] = useState<User[]>([]); 
 
   useEffect(() => {
-    document.title = "My App";
-  });
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    .then(res => setUsers(res.data));
+  }, [])
 
   return (
-    <div>
-      <select  className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
+   <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
   );
 }
 
