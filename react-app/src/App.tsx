cf81a04922757241acdf75409fe5871269d76325
josuerushanika <josuerushanika@gmail.com>
2023-09-12 10:41:11 +0200

    Update files

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,13 +3,11 @@ import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
-
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
--- react-app/src/components/ExpenseForm.tsx
+++ react-app/src/components/ExpenseForm.tsx
@@ -1,29 +1,44 @@
-import React from "react";
-import { categories } from "../App";
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import categories from "../categories";
+
+
+const schema = z.object({
+    description : z.string().min(3).max(50),
+    amount: z.number().min(0.01).max(100_000),
+    category: z.enum(categories),
+});
+type ExpenseForm = z.infer<typeof schema>;
 
 const ExpenseForm = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm<ExpenseFormData>({ resolver: zodResolver(schema)})
+
   return (
     <form>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input id="description" type="text" className="form-control" />
+        <input {...register('description')} id="description" type="text" className="form-control" />
+        {errors.description && <p className='text-danger'>{errors.description.message}</p>}
       </div>
 
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input id="amount" type="number" className="form-control" />
+        <input  {...register('amount')}  id="amount" type="number" className="form-control" />
+        {errors.amount && <p className='text-danger'>{errors.amount.message}</p>}
       </div>
 
       <div className="mb-3">
         <label htmlFor="category" className="form-label">Category</label>
-        <select name="" id="category" className="form-select">
+        <select {...register('category')}  id="category" className="form-select">
             <option value=""></option>
             {categories.map(category => <option key={category} value={category}>{category}</option>)}
         </select>
+        {errors.category && <p className='text-danger'>{errors.category.message}</p>}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
